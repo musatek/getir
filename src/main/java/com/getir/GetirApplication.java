c20e5be0609f4b59cba4f98dc745aa7eb1f55c15
Musa TEK <musatek58@gmail.com>
2021-08-31 21:43:27 +0300

    Initial commit

---
+++ src/main/java/com/getir/GetirApplication.java
@@ -0,0 +1,13 @@
+package com.getir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetirApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetirApplication.class, args);
+    }
+
+}
